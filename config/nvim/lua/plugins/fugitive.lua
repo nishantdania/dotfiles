55d0308d556e9d882ff421b8270defc025b859ec
nishantdania <nishantdania@gmail.com>
2025-02-02 13:35:43 +0800

    Make rhubarb a dep of fugitive

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,4 +1,5 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = { "tpope/vim-rhubarb" },
 	config = function() end,
 }
--- config/nvim/lua/plugins/vim-rhubarb.lua
+++
@@ -1,4 +0,0 @@
-return {
-	"tpope/vim-rhubarb",
-	config = function() end,
-}
